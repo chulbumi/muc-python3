da28f4ac29e2d1adfd6f04ccaeda79104dd13fa6
Ubuntu <muc.renewal@gmail.com>
2021-02-02 00:55:43 +0900

    item.py

--- objs/item.py
+++ objs/item.py
@@ -44,6 +44,9 @@ class Item(Object):
             return False
             
         self.inUse = False
+        react = self['반응이름']
+        if type(react) == str:
+            self['반응이름'] = [ react ]
         #print '%s 생성!!!' % str(index)
 
     def save(self, mode = True):
